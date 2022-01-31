2625fedba99d5764f7f78ecc15ae5f110e1f2832
JoelOgualiri <j.ogualiri@gmail.com>
2022-01-31 15:20:13 -0500

    skeleton functionality done

--- server/server.js
+++ server/server.js
@@ -24,7 +24,46 @@ app.get("/api/home", (req, res) => {
         res.send(response)
     })
 })
+app.post("/api/addBill", (req, res) => {
+    const billName = req.body.bill_name;
+    const billAmount = req.body.bill_amount;
+    const dueDate = req.body.due_date;
+    const reminderLength = req.body.reminder_length || null;
+    const reminderColor = req.body.reminder_color || null;
+    const billCurrency = req.body.bill_currency || '$';
 
+
+    const sqlInsert = "INSERT INTO billshub.bills (bill_name, bill_amount,due_date,reminder_length,reminder_color, bill_currency) VALUES (?,?,?,?,?,?)"
+    db.query(sqlInsert, [billName, billAmount, dueDate, reminderLength, reminderColor, billCurrency], (err, res) => {
+        if (err) console.error(err)
+    });
+    res.send("new bill added successfully")
+})
+
+app.delete("/api/deleteBill/:bill_id", (req, res) => {
+    const billID = req.params.bill_id
+    const sqlDelete = `DELETE FROM billshub.bills WHERE bill_id = ?`;
+    db.query(sqlDelete, [billID], (err, result) => {
+        if (err) return console.error(err.message);
+        res.send('bill deleted successfully')
+    })
+})
+
+app.put("/api/updateBill", (req, res) => {
+    const billID = req.body.bill_id;
+    const billName = req.body.bill_name;
+    const billAmount = req.body.bill_amount;
+    const dueDate = req.body.due_date;
+    const reminderLength = req.body.reminder_length || null;
+    const reminderColor = req.body.reminder_color || null;
+    const billCurrency = req.body.bill_currency || '$';
+    const sqlUpdate = "UPDATE billshub.bills SET bill_name = ?, bill_amount = ?, due_date = ?, reminder_length =?, reminder_color =?,  bill_currency = ? WHERE bill_id = ?";
+    db.query(sqlUpdate, [billName, billAmount, dueDate, reminderLength, reminderColor, billCurrency, billID], (err, res) => {
+        if (err) console.error(err.message);
+
+    });
+    res.send("bill updated successfully")
+})
 app.listen(port, () => {
     console.log(`running on port: ${port}`)
 })
